5e175e02d180da62f7640a70604393643f09752a
James Lee <uptownhr@gmail.com>
2016-02-29 19:24:22 -0800

    removed unused api. created single oauth login callback

--- controllers/home.js
+++ controllers/home.js
@@ -7,7 +7,6 @@ const Template = require('../models/Template')
  */
 exports.index = function(req, res) {
   Template.findOne((err,template) => {
-    console.log(template, template.full_name)
     res.render('home-1-static-image', {
       title: 'Home',
       template: template
